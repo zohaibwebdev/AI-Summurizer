ea1e8f0a78a5a668c8a612389b452066cf42a10e
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2024-05-05 13:17:44 +0500

    feat: prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
-        "next/core-web-vitals"
+        "next/core-web-vitals",
+        'plugin:prettier/recommended',
     ],
     'overrides': [
         {
