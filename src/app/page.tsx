07f35770ab21eb6ef07a22f12288aa134345f5c0
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2024-05-08 16:14:49 +0500

    feat: Header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import Navigation from '../shared/components/navigation/navigation.component'
-
+import Header from '@/shared/components/header/header.component'
 const page = () => {
     return (
         <div className="h-[100vh]">
             <Navigation />
+            <Header />
         </div>
     )
 }
---
+++ src/shared/components/header/header.component.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className="text-4xl capitalize text-center mb-6 text-gray-800 font-bold">
+                Summarize Articles with <br className="max-md:hidden" />
+                <span className="bg-gradient-to-r from-orange-600 via-purple-800 to-indigo-400 bg-clip-text text-transparent">
+                    OpenAI GPT-4
+                </span>
+            </h1>
+            <h2 className="text-center font-semibold leading-4 text-gray-600">
+                Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles
+                into clear and concise summaries
+            </h2>
+        </header>
+    )
+}
+
+export default Header
