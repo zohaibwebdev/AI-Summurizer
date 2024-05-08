2af329a9bf8fac53a3abb8b5854a46e7c1706a88
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2024-05-08 14:08:59 +0500

    fix: tailwindcss

--- shared/components/navigation/navigation.component.tsx
+++ shared/components/navigation/navigation.component.tsx
@@ -1,8 +1,10 @@
 import LogoIcon from '../../icons/logo.icon'
 const Navigation = () => {
     return (
-        <div className="w-[100vw] px-10 py-3">
+        <div className="w-[100vw] px-10 py-3 text-red-700">
+            <h1 className="text-purple-600">hello</h1>
             <nav className="flex justify-between items-center">
+                <h1 className="text-green-700">inside flex</h1>
                 <LogoIcon height={130} width={120} />
                 <button
                     onClick={() => window.open('https://github.com/zohaibwebdev', '_blank')}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navigation from '../../shared/components/navigation/navigation.component'
 const page = () => {
     return (
         <div className="h-[100vh]">
+            <h1 className="text-4xl text-red-600">test</h1>
             <Navigation />
         </div>
     )
