3c9166ff5d640a73e24d82c5ba93b9cd8163e0b4
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2024-05-08 14:31:07 +0500

    fix: tailwindcss error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 'use client'
-import Navigation from '../../shared/components/navigation/navigation.component'
+import Navigation from '../shared/components/navigation/navigation.component'
 
 const page = () => {
     return (
         <div className="h-[100vh]">
-            <h1 className="text-4xl text-red-600">test</h1>
             <Navigation />
         </div>
     )
--- src/shared/components/navigation/navigation.component.tsx
+++ src/shared/components/navigation/navigation.component.tsx
@@ -1,10 +1,8 @@
 import LogoIcon from '../../icons/logo.icon'
 const Navigation = () => {
     return (
-        <div className="w-[100vw] px-10 py-3 text-red-700">
-            <h1 className="text-purple-600">hello</h1>
+        <div className="w-[100vw] px-10 py-3">
             <nav className="flex justify-between items-center">
-                <h1 className="text-green-700">inside flex</h1>
                 <LogoIcon height={130} width={120} />
                 <button
                     onClick={() => window.open('https://github.com/zohaibwebdev', '_blank')}
