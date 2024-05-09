ca5f4f4763a3a91327d9a0b395e0c265342c288e
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2024-05-09 12:16:50 +0500

    fix: some code improvments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import CustomInput from '@/shared/components/custom-input/custom-input.component'
 import Navigation from '../shared/components/navigation/navigation.component'
 import Header from '@/shared/components/header/header.component'
 const page = () => {
@@ -6,6 +7,7 @@ const page = () => {
         <div className="h-[100vh] flex flex-col items-center justify-start gap-5 max-w-7xl">
             <Navigation />
             <Header />
+            <CustomInput placeholder="Enter Link here" type="url" required />
         </div>
     )
 }
--- src/shared/components/custom-input/custom-input.component.tsx
+++ src/shared/components/custom-input/custom-input.component.tsx
@@ -1,5 +1,17 @@
 import { FC } from 'react'
-import { InputPropsTypes } from '@/shared/types/input.types'
+interface InputPropsTypes {
+    label?: string
+    type?: 'url' | 'text' | 'email' | 'password' | 'number'
+    placeholder?: string
+    required?: boolean
+    readonly?: boolean
+    name?: string
+    className?: string
+    changeEvent?: (e: React.ChangeEvent<HTMLInputElement>) => void
+    defaultValue?: string | number
+    errorMessage?: string
+    labelClasses?: string
+}
 
 const CustomInput: FC<InputPropsTypes> = ({
     label,
@@ -12,13 +24,14 @@ const CustomInput: FC<InputPropsTypes> = ({
     className,
     changeEvent,
     readonly,
+    labelClasses,
 }) => {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         changeEvent?.(e)
     }
     return (
-        <div>
-            {label && <label>{label}:</label>}
+        <div className="flex flex-col gap-1 items-center">
+            {label && <label className={labelClasses}>{label}:</label>}
             <input
                 name={name}
                 type={type}
