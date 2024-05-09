4c3efb17e013b4bc89b573ab347afd1b2f4d13ea
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2024-05-09 20:38:34 +0500

    fix: improve button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,11 @@ import CustomButton from '@/shared/components/custom-button/custom-button.compon
 import Navigation from '../shared/components/navigation/navigation.component'
 import Header from '@/shared/components/header/header.component'
 const page = () => {
-    const handleClick = () => {
-        console.log('click')
-    }
     return (
         <div className="h-[100vh] flex flex-col items-center justify-start gap-5 max-w-7xl">
             <Navigation />
             <Header />
-            <CustomButton value="submit" onClick={handleClick} />
+            <CustomButton value="submit" />
         </div>
     )
 }
