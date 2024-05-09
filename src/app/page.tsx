dc5dba36d6a70289b2967a3aa061d503fb7c038f
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2024-05-09 12:49:07 +0500

    fix: some code improvments with types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import CustomInput from '@/shared/components/custom-input/custom-input.component'
 import Navigation from '../shared/components/navigation/navigation.component'
 import Header from '@/shared/components/header/header.component'
 const page = () => {
@@ -7,7 +6,6 @@ const page = () => {
         <div className="h-[100vh] flex flex-col items-center justify-start gap-5 max-w-7xl">
             <Navigation />
             <Header />
-            <CustomInput placeholder="Enter Link here" type="url" required />
         </div>
     )
 }
--- src/shared/components/custom-input/custom-input.component.tsx
+++ src/shared/components/custom-input/custom-input.component.tsx
@@ -1,17 +1,5 @@
 import { FC } from 'react'
-interface InputPropsTypes {
-    label?: string
-    type?: 'url' | 'text' | 'email' | 'password' | 'number'
-    placeholder?: string
-    required?: boolean
-    readonly?: boolean
-    name?: string
-    className?: string
-    changeEvent?: (e: React.ChangeEvent<HTMLInputElement>) => void
-    defaultValue?: string | number
-    errorMessage?: string
-    labelClasses?: string
-}
+import { InputPropsTypes } from './custom-input.types'
 
 const CustomInput: FC<InputPropsTypes> = ({
     label,
